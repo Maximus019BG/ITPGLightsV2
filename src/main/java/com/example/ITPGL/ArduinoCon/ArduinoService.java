125acc596c844105d15651fb044545b7d45f2291
Maximus019BG <maxralev@abv.bg>
2024-04-09 12:13:40 +0300

    Fix(arduino)bug with pins and buttons

--- src/main/java/com/example/ITPGL/ArduinoCon/ArduinoService.java
+++ src/main/java/com/example/ITPGL/ArduinoCon/ArduinoService.java
@@ -1,48 +1,23 @@
 package com.example.ITPGL.ArduinoCon;
 
-import com.fazecast.jSerialComm.SerialPort;
-import org.springframework.beans.factory.annotation.Value;
+import com.fazecast.jSerialComm.*;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ArduinoService {
+
     private SerialPort comPort;
 
-    public ArduinoService(@Value("${arduino.port-description}") String portDescription) {
-        comPort = SerialPort.getCommPort(portDescription);
-        comPort.setBaudRate(9600);
+    public ArduinoService() {
+        comPort = SerialPort.getCommPorts()[0];
         comPort.openPort();
-    }
-    public void turnOnLampSet1() {
-        sendCommand('1');
+        comPort.setBaudRate(9600);
     }
 
-    public void turnOnLampSet2() {
-        sendCommand('2');
+    public void sendCommand(char command) {
+        byte[] commandBytes = new byte[]{(byte) command};
+        comPort.writeBytes(commandBytes, 1);
+
+        System.out.println("Command sent: " + command);
     }
-
-    public void turnOnLampSet3() {
-        sendCommand('3');
-    }
-
-    public void turnOnLampSet4() {
-        sendCommand('4');
-    }
-
-    public void stopLampSet() {
-        sendCommand('0');
-
-    }
-
-    private void sendCommand(char command) {
-        comPort.writeBytes(new byte[] {(byte) command}, 1);
-    }
-
-
-
-    public void close() {
-        comPort.closePort();
-    }
-
-
 }
