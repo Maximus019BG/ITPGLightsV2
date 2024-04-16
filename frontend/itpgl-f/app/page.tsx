6271ad45c2a14305118edf1e8e888fed5f67e8c7
SimoSabev <146847064+SimoSabev@users.noreply.github.com>
2024-04-16 19:00:47 +0300

    Update page.tsx

--- frontend/itpgl-f/app/page.tsx
+++ frontend/itpgl-f/app/page.tsx
@@ -3,18 +3,20 @@ import React from 'react';
 import Header from "./Components/header/page";
 import Buttons from "./Components/buttons/page";
 import Map from "./Components/map/page";
-import Bulding from "./Components/building/page";
+// import Video from './Components/video/page';
 
 
 
 
 const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 border-8 border-sky-500 overflow-hidden">
+    <main className="flex min-h-screen flex-col items-end justify-center p-24 border-8 border-sky-500 overflow-hidden">
+      <div className=' flex flex-col justify-center items-center'>
         <Header />
         <Buttons />
+      </div>
         <Map />
-        <Bulding />
+        {/* <Video /> */}
     </main>
   );
 }
