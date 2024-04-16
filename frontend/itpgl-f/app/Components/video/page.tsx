154a05ebf794d6dd89464adc9496cad0c9c76c91
SimoSabev <146847064+SimoSabev@users.noreply.github.com>
2024-04-16 19:04:44 +0300

    Add files via upload

---
+++ frontend/itpgl-f/app/Components/video/page.tsx
@@ -0,0 +1,7 @@
+// import React from 'react';
+// import Video from 'next-video';
+//   import getStarted from '/videos/get-started.mp4';
+
+//   export default function Video() {
+//     // return <Video src={getStarted} />;
+//   }
